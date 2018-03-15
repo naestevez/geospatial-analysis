b9fed5e9db62a8b52cc23ebe4ed9fba60cd3748e
Nayeli Alejandra Estevez <nestevez7@gmail.com>
2018-03-15 23:12:06 +0000

    changed syntax with how modules were being imported, changed the visible extent to match a feature's bounding box

--- mapnik/shapeToMap.py
+++ mapnik/shapeToMap.py
@@ -1,13 +1,37 @@
 import mapnik
+from osgeo import gdal, ogr
+from shapely import geometry
+
+countryChosen = raw_input('Which country would you like to see? ')
+countryChosen = countryChosen.lower()
+print countryChosen
+
+#find bounding box for a specific feature
+shapefile = ogr.Open("TM_WORLD_BORDERS-0.3.shp")
+layer = shapefile.GetLayer(0)
+
+for i in range(layer.GetFeatureCount()):
+    feature = layer.GetFeature(i)
+    feature_name = feature.GetField("NAME")
+    feature_name = feature_name.lower()
+    if feature_name == countryChosen:
+        geometry = feature.GetGeometryRef()
+        wkt = geometry.ExportToWkt()
+        outline = shapely.wkt.loads(wkt)
+        print type(outline.bounds) #(min_x, min_y, max_x, max_y)
+
+#assign bounds of specific country to visible extent of map
+(BOUNDS_MIN_LONG, BOUNDS_MIN_LAT, BOUNDS_MAX_LONG, BOUNDS_MAX_LAT) = outline.bounds
+
 stylesheet = 'shapeToMap.xml'
 
 BACKGROUND_COLOR = '#a0c0ff'
 
 #variables for visible extent of map
-BOUNDS_MIN_LAT = 35.26
-BOUNDS_MAX_LAT = 71.39
-BOUNDS_MIN_LONG = -10.90
-BOUNDS_MAX_LONG = 41.13
+# BOUNDS_MIN_LAT = 35.26
+# BOUNDS_MAX_LAT = 71.39
+# BOUNDS_MIN_LONG = -10.90
+# BOUNDS_MAX_LONG = 41.13
 
 #max size of image - might be smaller than bounding rectangle
 MAX_WIDTH = 1600
