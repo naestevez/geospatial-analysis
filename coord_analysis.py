68c8586c95c9d6cfd7f40fdbdde0d3c4839dc59f
Nayeli Alejandra Estevez <nestevez7@gmail.com>
2018-03-29 12:40:41 +0100

    calculate ending coordinates and inverse heading by specifying starting coordinate, distance and heading

--- coord_analysis.py
+++ coord_analysis.py
@@ -17,12 +17,32 @@ def get_coord(prompt):
             continue
         return latitude, longitude
 
-lat1,long1 = get_coord("Starting coordinate")
-lat2,long2 = get_coord("Ending coordinate")
+# lat1,long1 = get_coord("Starting coordinate")
+# lat2,long2 = get_coord("Ending coordinate")
+#
+# #calculate distance between these two coordinates
+# heading1,heading2,distance = geod.inv(long1, lat1, long2, lat2)
+#
+# print "Heading = %0.2f degrees" % heading1
+# print "Inverse heading = %0.2f degrees" % heading2
+# print "Distance = %0.2f kilometers" % (distance/1000)
 
-#calculate distance between these two coordinates
-heading1,heading2,distance = geod.inv(long1, lat1, long2, lat2)
+def get_num(prompt):
+    while True:
+        s = raw_input(prompt + ": ")
+        try:
+            value = float(s)
+        except ValueError:
+            continue
+        return value
 
-print "Heading = %0.2f degrees" % heading1
-print "Inverse heading = %0.2f degrees" % heading2
-print "Distance = %0.2f kilometers" % (distance/1000)
+#prompts user for starting coordinate, distance and heading
+sLat, sLong = get_coord("Starting coordinate")
+distance = get_num("Distance in kilometers") * 1000
+heading = get_num("Heading")
+
+#calculate ending coordinate and inverse heading and print it out
+eLong, eLat, iHeading = geod.fwd(sLong, sLat, heading, distance)
+
+print "End point = (%0.4f,%0.4f)" % (eLat, eLong)
+print "Inverse heading = %0.2f degrees" % iHeading
